e15f70f1bd84fbc0bd7c499ac6b310183adf1f81
WINDOWS 10 <haliferliawan4@gmail.com>
2022-07-01 02:33:54 +0700

    commit 3

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import bodyParser from 'body-parser';
 import dataRoutes from './routes/pesawat.js';
+let port = process.env.PORT || 3000;
 
 const app = express();
-const PORT = 5000;
+
 
 app.use(bodyParser.json());
 app.use('/hotel', dataRoutes);
@@ -12,4 +13,4 @@ app.get('/', (req,res)=>{
     res.send('Halo');
 });
 
-app.listen(PORT, ()=> console.log(`Server Telah berjalan di port ${PORT}`))
+app.listen(port, ()=> console.log(`Server Telah berjalan di port http://localhost:${port}`))
