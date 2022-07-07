a782f155da59dda8d98e778761309a565f69f282
ridhaagam <muhridhaagam@gmail.com>
2022-07-07 18:45:02 +0700

    fixed

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import express from "express";
 import bodyParser from 'body-parser';
-import dataRoutes from './routes/pesawat.js';
+import dataRoutes from './routes/sepatu.js';
 let port = process.env.PORT || 3000;
 
 const app = express();
 
 
 app.use(bodyParser.json());
-app.use('/pesawat', dataRoutes);
+app.use('/sepatu', dataRoutes);
 
 app.get('/', (req,res)=>{
     res.send('Halo');
